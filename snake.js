f08384c7477ea92e534ed593482ad509e0b52015
leegopc <dzzzzzb@gmail.com>
2020-09-20 03:50:58 +0800

    add score/move

--- snake.js
+++ snake.js
@@ -26,6 +26,21 @@ y : Math.floor(Math.random()*15+3) * box,
 
 let score = 0;
 
+document.addEventListener('keydown', direction);
+
+let d;
+function direction(event){
+    if(event.keyCode == 37 && d != "RIGHT"){
+        d = "LEFT";
+    }else if(event.keyCode == 38 && d != "DOWN"){
+        d = "UP";
+    }else if(event.keyCode == 39 && d != "LEFT"){
+        d = "RIGHT";
+    }else if(event.keyCode == 40 && d != "UP"){
+        d = "DOWN";
+    }
+}
+
 
 
 function draw(){
@@ -36,7 +51,59 @@ function draw(){
         ctx.strokeStyle = "white";
         ctx.strokeRect(snake[i].x,snake[i].y,box,box);
     }
-    ctx.drawImage(foodImg, food.x, food.y)
+
+    ctx.drawImage(foodImg, food.x, food.y);
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(snakeX == food.x && snakeY == food.y){
+        score ++;
+        food = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box,
+        }
+    }else{
+            snake.pop();
+        }
+
+
+   
+    
+
+    if(d == "LEFT") snakeX -= box;
+    if(d == "UP") snakeY -= box;
+    if(d == "RIGHT") snakeX += box;
+    if(d == "DOWN") snakeY += box;
+
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+
+    if(snakeX < box || snakeX > 17*box || snakeY < 3 *box || snakeY > 17* box || collision(newHead, snake)){
+        clearInterval(game);
+    }
+    
+    function collision(head, array){
+        for( let i = 0; i<array.length; i++){
+            if(head.x == array[i].x && head.y == array[i].y){
+                return true;
+            }
+        }
+    }
+
+
+    snake.unshift(newHead);
+
+    
+  
+
+    
+
+    ctx.fillStyle = "white";
+    ctx.font = "50px Changa one";
+    ctx.fillText(score, 3*box,1.6*box);
 }
 
 let game = setInterval(draw,100);
